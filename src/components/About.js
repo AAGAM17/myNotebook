f17be2a0386c54d6a2d1bc952089469f239bc74a
AAGAM SHAH <aagamcshah172005@gmai;.com>
2022-02-24 17:10:17 +0530

    myNotebook Complete

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,8 @@ import React from "react";
 const About = () => {
     return ( 
         <div>
-           This is About
+          <h1>This is myNoteBook. </h1>
+           <h2>myNoteBook is a Secure and Simple to use Note taking app</h2>
         </div>
     )
 };
